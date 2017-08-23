a0d0e04463bb5cdb7c70851776b67f551a64346d
ramastein <rams.rvp@gmail.com>
2017-08-24 00:09:06 +0530

    Add files via upload

--- loginhint.js
+++ loginhint.js
@@ -9,5 +9,18 @@ node.insertBefore(hint, node.firstChild);
 document.body.style.backgroundImage = "url(https://rawgit.com/ramastein/test/master/FDBECT.jpg)";
 document.getElementsByTagName('table')[0].cellSpacing ="12px";
 document.getElementById("breadcrumbBar").style.display = "none";
+document.getElementById("jenkins-head-icon").style.display = "none";
+document.getElementById("searchform").style.display = "none";
+document.getElementById("jenkins-name-icon").style.width = "174px";
+document.getElementsByTagName("footer")[0].style.display="none";
+ADD <div style="
+    display: block;margin-left: 233px;margin-top: 20px;font-size: 20px;text-transform: none;
+">Robotics Process Automation Tool</div>
+
+var para=document.createElement("div");
+var node=document.createTextNode("Robotic Process Automation Tool");
+para.appendChild(node);
+para.setAttribute("style","display: block;margin-left: 233px;margin-top: 20px;font-size: 20px;text-transform: none;");
+document.getElementById("jenkins-home-link").appendChild(para);
 }
 });}}
