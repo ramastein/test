1ce3373e3034876c668231be66636837e642aeab
ramastein <rams.rvp@gmail.com>
2017-08-23 20:00:41 +0530

    Add files via upload

--- loginhint.js
+++ loginhint.js
@@ -1,4 +1,12 @@
 {{document.addEventListener('DOMContentLoaded', function() {
 if(window.location.href.indexOf("/login?from=") !== -1)
-{ var hint=document.createTextNode("Hint: Use your XXXXXX credentials to login."); var node = document.getElementById("main-panel"); node.insertBefore(hint, node.firstChild); }
+{
+/*var hint=document.createTextNode("Hint: Use your XXXXXX credentials to login."); 
+var node = document.getElementById("main-panel"); 
+node.insertBefore(hint, node.firstChild); 
+*/
+
+document.body.style.backgroundImage = "url(https://rawgit.com/ramastein/test/master/FDBECT.jpg)";
+
+}
 });}}
